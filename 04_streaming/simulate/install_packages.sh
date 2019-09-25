825cea610342ac780355adfe44d70397c5d4e0af
hi32ce <hi32ce_2@hotmail.com>
2019-09-26 04:29:23 +0900

    Update script

--- 04_streaming/simulate/install_packages.sh
+++ 04_streaming/simulate/install_packages.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-sudo pip install --upgrade timezonefinder==3.0.0 pytz 'apache-beam[gcp]'
+conda install pytz
+pip install timezonefinder==3.0.0
+conda install "PyYAML<4.0.0,>=3.12"
+pip install 'apache-beam[gcp]'
